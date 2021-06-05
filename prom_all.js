a458cc009150319464cad2b367b5a38057da3af9
KennethAbreu <kennethabreu1@gmail.com>
2021-06-04 23:02:21 -0400

    Commit test 2

--- prom_all.js
+++ prom_all.js
@@ -16,4 +16,5 @@ Promise.all([promise1, promise2, promise3]).then((res) => {
     console.log(res[0]);
     console.log(res[1]);
     console.log(res[2]);
+    console.log("We've successfully logged 3,2,1 in sequential order using promises!")
 })
